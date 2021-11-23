b2349e72c2980f7534676a9936cf5643fbc6c58d
Grazvydas-M <m.grazvydas@yahoo.com>
2021-11-23 14:15:07 +0200

    add cache

--- app/Services/NewsServices.php
+++ app/Services/NewsServices.php
@@ -2,10 +2,13 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Cache;
 use jcobhams\NewsApi\NewsApi;
 
 class NewsServices
 {
+    private const ARTICLES_KEY = 'articles';
+    private const SOURCES_KEY = 'sources';
     public NewsApi $newsApi;
 
     public function __construct(NewsApi $newsApi)
@@ -15,14 +18,22 @@ class NewsServices
 
     public function getSources(): array
     {
+        if (Cache::has(self::SOURCES_KEY)) {
+            return Cache::get(self::SOURCES_KEY, []);
+        }
         $sources[] = $this->newsApi->getSources(null, 'en')->sources;
+        Cache::put(self::SOURCES_KEY, $sources, 3600);
 
         return $sources;
     }
 
     public function getArticles(string $source, string $title = null): array
     {
+        if (Cache::has(self::ARTICLES_KEY . $source . $title)) {
+            return Cache::get(self::ARTICLES_KEY . $source . $title, []);
+        }
         $articles[] = $this->newsApi->getEverything($title, $source)->articles;
+        Cache::put(self::ARTICLES_KEY . $source . $title, $articles, 600);
 
         return $articles;
     }
